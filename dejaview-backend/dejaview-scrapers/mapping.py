e4698bda2fb72d440f69ac49d3040458366c19c6
Unknown <david.burgess.au@gmail.com>
2018-02-15 15:50:18 +1100

    Updated Google Maps
    Now with street view

--- dejaview-backend/dejaview-scrapers/mapping.py
+++ dejaview-backend/dejaview-scrapers/mapping.py
@@ -1,38 +1,15 @@
-import googlemaps, requests, json
-from datetime import datetime
+import googlemaps, requests
 
 gmaps = googlemaps.Client(key='AIzaSyCPNuuW1GALn89xaVYxuLzuN3JELL0ClLk')
-
 geocode_result = gmaps.geocode('20 Haines Street, Curtin, ACT, 2605')
-
-# Geocoding an address
-#geocode_result = gmaps.geocode('1600 Amphitheatre Parkway, Mountain View, CA')
-
-print ('Geocode results')
-
-print (geocode_result[0].keys())
-
-print ("Lat:  " + str(geocode_result[0].get("geometry").get("location").get("lat")))
-print ("Long: " + str(geocode_result[0].get("geometry").get("location").get("lng")))
 
 targetLat = geocode_result[0].get("geometry").get("location").get("lat")
 targetLong = geocode_result[0].get("geometry").get("location").get("lng")
+targetAddress = str(geocode_result[0].get("formatted_address"))
 
+print ('Collecting Google Maps imagery...')
 
-# Look up an address with reverse geocoding
-#reverse_geocode_result = gmaps.reverse_geocode((40.714224, -73.961452))
-
-
-
-# Request directions via public transit
-#now = datetime.now()
-#directions_result = gmaps.directions("Sydney Town Hall",
-#                                     "Parramatta, NSW",
-#                                     mode="transit",
-#                                     departure_time=now)
-#print ('Direction result')
-#print directions_result
-#Get Target Road Map
+#Get Local Road Map
 pic_url = ''.join(['https://maps.googleapis.com/maps/api/staticmap?center=', str(targetLat), ',', str(targetLong), '&zoom=18&size=640x640&maptype=roadmap'])
 with open('googleLocalRoadMap.jpg', 'wb') as handle:
     response = requests.get(pic_url, stream=True)
@@ -44,19 +21,21 @@ with open('googleLocalRoadMap.jpg', 'wb') as handle:
             break
 
         handle.write(block)
+    print('  Target Street Map Downloaded')
 
-    # Get Suburb Road Map
-    pic_url = ''.join(['https://maps.googleapis.com/maps/api/staticmap?center=', str(targetLat), ',', str(targetLong),'&zoom=15&size=640x640&maptype=roadmap'])
-    with open('googleSuburbRoadMap.jpg', 'wb') as handle:
-        response = requests.get(pic_url, stream=True)
-        if not response.ok:
-            print response
+# Get Suburb Road Map
+pic_url = ''.join(['https://maps.googleapis.com/maps/api/staticmap?center=', str(targetLat), ',', str(targetLong),'&zoom=15&size=640x640&markers=color:red%7C', str(targetLat), ',', str(targetLong),'&maptype=roadmap'])
+with open('googleSuburbRoadMap.jpg', 'wb') as handle:
+    response = requests.get(pic_url, stream=True)
+    if not response.ok:
+        print response
 
-        for block in response.iter_content(1024):
-            if not block:
-                break
+    for block in response.iter_content(1024):
+        if not block:
+            break
 
-            handle.write(block)
+        handle.write(block)
+    print('  Suburb Street Map Downloaded')
 
 # Get Target Overhead Imagery
 pic_url = ''.join(['https://maps.googleapis.com/maps/api/staticmap?center=', str(targetLat), ',', str(targetLong),'&zoom=20&size=640x640&maptype=satellite'])
@@ -70,9 +49,10 @@ with open('googleTargetSatellite.jpg', 'wb') as handle:
             break
 
         handle.write(block)
+    print('  Target Overhead Imagery Downloaded')
 
 # Get Area Imagery
-pic_url = ''.join(['https://maps.googleapis.com/maps/api/staticmap?center=', str(targetLat), ',', str(targetLong),'&zoom=18&size=640x640&maptype=hybrid'])
+pic_url = ''.join(['https://maps.googleapis.com/maps/api/staticmap?center=', str(targetLat), ',', str(targetLong),'&zoom=18&size=640x640&markers=color:red%7C', str(targetLat), ',', str(targetLong),'&maptype=hybrid'])
 with open('googleAreaImagery.jpg', 'wb') as handle:
     response = requests.get(pic_url, stream=True)
     if not response.ok:
@@ -83,3 +63,20 @@ with open('googleAreaImagery.jpg', 'wb') as handle:
             break
 
         handle.write(block)
+    print('  Area Imagery Downloaded')
+
+# Get Street View
+pic_url = ''.join(['https://maps.googleapis.com/maps/api/streetview?size=680x400&location=', str(targetAddress),'&fov=110&heading=235&pitch=0&key=AIzaSyCPNuuW1GALn89xaVYxuLzuN3JELL0ClLk'])
+pic_url = pic_url.replace(' ', '%20')
+
+with open('googleStreetView.jpg', 'wb') as handle:
+    response = requests.get(pic_url, stream=True)
+    if not response.ok:
+        print response
+
+    for block in response.iter_content(1024):
+        if not block:
+            break
+
+        handle.write(block)
+    print('  Street View Downloaded')
