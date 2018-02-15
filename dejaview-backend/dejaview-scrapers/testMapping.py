14f3d7fc860a637864fc2ef0d1de7647eb3cbdce
Unknown <david.burgess.au@gmail.com>
2018-02-15 23:10:22 +1100

    Mapping Update

--- dejaview-backend/dejaview-scrapers/testMapping.py
+++ dejaview-backend/dejaview-scrapers/testMapping.py
@@ -1,3 +1,4 @@
 import mapping
 
-mapping.getImagery('20 Haines Street, Curtin, ACT, 2605')
+#mapping.getImagery('20 Haines Street, Curtin, ACT, 2605')
+mapping.getImagery('5 Duffy St, Ainslie, ACT, 2602')
